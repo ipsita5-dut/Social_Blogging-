4d5d54903b7c664fdab078c127df45d54edee580
Dp-052004 <121806338+Dp-052004@users.noreply.github.com>
2025-03-31 19:50:17 +0530

    Add files via upload

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { AuthProvider } from "./context/AuthContext";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <AuthProvider>
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+    </AuthProvider>
+  );
+}
